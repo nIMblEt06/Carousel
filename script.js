bb5bc5134489961e409396b41602367e18de3418
Shwetabh Niket <f20210923@pilani.bits-pilani.ac.in>
2022-04-06 04:00:29 +0530

    1024x768 compatibility

--- script.js
+++ script.js
@@ -8,7 +8,17 @@ let nameCostaa = document.querySelector(".name");
 let dept = document.querySelector(".dept");
 let topRight = document.querySelector(".topRight");
 let gear = document.querySelector(".gear");
+var ht = window.innerHeight;
+var width = window.innerWidth;
 
+let theta = width / ht;
+let inverse = Math.atan(theta);
+let divHt = (ht*ht + width*width)^1/2;
+console.log(divHt);
+// window.addEventListener('load', () => {
+//     downSlide.style.transform = `rotate(-${inverse}rad) translate(1170px,-${(slidesLength - 1 - currentSlide) * 3800}px)`;
+//     upSlide.style.transform = `rotate(-${inverse}rad) translate(1170px, -${currentSlide * 3800}px)`;
+// })
 window.addEventListener('keydown', e => {
     if (e.key == "ArrowDown") {
         currentSlide++;
@@ -16,8 +26,8 @@ window.addEventListener('keydown', e => {
         if (currentSlide > slidesLength - 1) {
             currentSlide = 0;
         }
-        downSlide.style.transform = `rotate(-64deg) translate(1470px,-${(slidesLength - 1 - currentSlide) * 3800}px)`;
-        upSlide.style.transform = `rotate(-64deg) translate(1470px, -${currentSlide * 3800}px)`;
+        downSlide.style.transform = `rotate(-${inverse}rad) translate(1170px,-${(slidesLength - 1 - currentSlide) * 3800}px)`;
+        upSlide.style.transform = `rotate(-${inverse}rad) translate(1170px, -${currentSlide * 3800}px)`;
         update();
     }
 })
@@ -29,105 +39,94 @@ window.addEventListener('keydown', e => {
             currentSlide = slidesLength - 1;
         }
         update();
-        downSlide.style.transform = `rotate(-64deg) translate(1470px,-${(slidesLength - 1 - currentSlide) * 3800}px)`;
-        upSlide.style.transform = `rotate(-64deg) translate(1470px,-${currentSlide * 3800}px)`;
+        downSlide.style.transform = `rotate(-${inverse}rad) translate(1170px,-${(slidesLength - 1 - currentSlide) * 3800}px)`;
+        upSlide.style.transform = `rotate(-${inverse}rad) translate(1170px,-${currentSlide * 3800}px)`;
     }
 })
-window.addEventListener('keydown', e => {
-    if (e.key == '1') {
-        currentSlide = 0;
+window.addEventListener('keydown', (e) => {
+
+    keyPressed = parseInt(e.key);
+    if (keyPressed >= 1 && keyPressed <= 9) {
+        currentSlide = keyPressed - 1;
     }
-    else if (e.key == '2') {
-        currentSlide = 1;
-    }
-    else if (e.key == '3') {
-        currentSlide = 2;
-    }
-    else if (e.key == '4') {
-        currentSlide = 3;
-    }
-    else if (e.key == '5') {
-        currentSlide = 4;
-    }
-    else if (e.key == '6') {
-        currentSlide = 5;
-    }
-    else if (e.key == '7') {
-        currentSlide = 6;
-    }
-    else if (e.key == '8') {
-        currentSlide = 7;
-    }
-    else if (e.key == '9') {
-        currentSlide = 8;
-    }
-    downSlide.style.transform = `rotate(-64deg) translate(1470px,-${(slidesLength - 1 - currentSlide) * 3800}px)`;
-    upSlide.style.transform = `rotate(-64deg) translate(1470px,-${currentSlide * 3800}px)`;
+
+    downSlide.style.transform = `rotate(-${inverse}rad) translate(1170px,-${(slidesLength - 1 - currentSlide) * 3800}px)`;
+    upSlide.style.transform = `rotate(-${inverse}rad) translate(1170px,-${currentSlide * 3800}px)`;
     update();
 })
-function update(){
-    if(currentSlide==0){
-    nameCostaa.textContent = "Priyanshu Shukla";
-    dept.textContent = "DEPARTMENT OF VISUAL MEDIA"
-    topRight.style.filter = "hue-rotate(0deg)"
-    gear.style.filter = "hue-rotate(0deg)"
-    photo.src = "Asset/Priyanshu.png"
 
-}
-else if(currentSlide==1){
-    nameCostaa.textContent = "Vidushi Sahay";
-    dept.textContent = "REGISTRATIONS & CORRESPONDENCE"
-    topRight.style.filter = "hue-rotate(90deg)"
-    gear.style.filter = "hue-rotate(90deg)"
-    photo.src = "Asset/Vidushi.png"
-} 
-else if(currentSlide==2){
-    nameCostaa.textContent = "Shashwath M S";
-    dept.textContent = "RECEPTION AND ACCOMODATION"
-    topRight.style.filter = "hue-rotate(200deg)"
-    gear.style.filter = "hue-rotate(190deg)"
-    photo.src = "Asset/Shashwath.png"
-} 
-else if(currentSlide==3){
-    nameCostaa.textContent = "Shreya Gupta";
-    dept.textContent = "SPONSORSHIP AND MARKETING"
-    topRight.style.filter = "hue-rotate(230deg)"
-    gear.style.filter = "hue-rotate(190deg)"
-    photo.src = "Asset/Shreya Gupta 1.png"
-} 
-else if(currentSlide==4){
-    nameCostaa.textContent = "Rhythm Saxena";
-    dept.textContent = "PUBLICITY AND ONLINE PARTNERSHIPS"
-    topRight.style.filter = "hue-rotate(235deg)"
-    gear.style.filter = "hue-rotate(240deg)"
-    photo.src = "Asset/Rhythm.png"
-} 
-else if(currentSlide==5){
-    nameCostaa.textContent = "Navya Bhatnagar";
-    dept.textContent = "EVENTS, PROJECTS AND LOGISTICS "
-    topRight.style.filter = "hue-rotate(190deg)"
-    gear.style.filter = "hue-rotate(195deg)"
-    photo.src = "Asset/Navya.png"
-} 
-else if(currentSlide==6){
-    nameCostaa.textContent = "Harsh Shukla";
-    dept.textContent = "PRESIDENT, STUDENT UNION"
-    topRight.style.filter = "hue-rotate(240deg)"
-    gear.style.filter = "hue-rotate(250deg)"
-    photo.src = "Asset/HarshShukla.png"
-} 
-else if(currentSlide==7){
-    nameCostaa.textContent = "Harsh Lamba ";
-    dept.textContent = "GENERAL SECRETARY, STUDENT UNION"
-    topRight.style.filter = "hue-rotate(200deg)"
-    gear.style.filter = "hue-rotate(200deg)"
-    photo.src = "Asset/HarshLamba.png"
-} 
-else if(currentSlide==8){
-    nameCostaa.textContent = "Divyam Chauhan";
-    dept.textContent = "GUEST LECTURES AND PAPER PRESENTATIONS"
-    topRight.style.filter = "hue-rotate(160deg)"
-    gear.style.filter = "hue-rotate(170deg)"
-    photo.src = "Asset/Divyam.png"
-} 
+data = [
+    {
+        "nameCostaa": "Priyanshu Shukla",
+        "dept": "DEPARTMENT OF VISUAL MEDIA",
+        "topRight": "hue-rotate(0deg)",
+        "gear": "hue-rotate(0deg)",
+        "photo": "Asset/Priyanshu.png"
+    },
+    {
+        "nameCostaa": "Vidushi Sahay",
+        "dept": "DEPARTMENT OF PUBLICATIONS AND CORRESPONDENCE",
+        "topRight": "hue-rotate(90deg)",
+        "gear": "hue-rotate(90deg)",
+        "photo": "Asset/Vidushi.png",
+    },
+    {
+        "nameCostaa": "Shashwath M S",
+        "dept": "DEPARTMENT OF RECEPTION AND ACCOMODATION",
+        "topRight": "hue-rotate(200deg)",
+        "gear": "hue-rotate(190deg)",
+        "photo": "Asset/Shashwath.png",
+    },
+    {
+        "nameCostaa": "Shreya Gupta",
+        "dept": "DEPARTMENT OF SPONSORSHIP AND MARKETING",
+        "topRight": "hue-rotate(230deg)",
+        "gear": "hue-rotate(190deg)",
+        "photo": "Asset/Shreya Gupta 1.png",
+    },
+    {
+        "nameCostaa": "Rhythm Saxena",
+        "dept": "DEPARTMENT OF ART, DESIGN, AND PUBLICITY",
+        "topRight": "hue-rotate(235deg)",
+        "gear": "hue-rotate(240deg)",
+        "photo": "Asset/Rhythm.png",
+    },
+    {
+        "nameCostaa": "Navya Bhatnagar",
+        "dept": "DEPARTMENT OF CONTROLS ",
+        "topRight": "hue-rotate(140deg)",
+        "gear": "hue-rotate(135deg)",
+        "photo": "Asset/Navya.png",
+    },
+    {
+        "nameCostaa": "Harsh Shukla",
+        "dept": "PRESIDENT, STUDENT UNION",
+        "topRight": "hue-rotate(240deg)",
+        "gear": "hue-rotate(250deg)",
+        "photo": "Asset/HarshShukla.png",
+    },
+    {
+        "nameCostaa": "Harsh Lamba ",
+        "dept": "GENERAL SECRETARY, STUDENT UNION",
+        "topRight": "hue-rotate(200deg)",
+        "gear": "hue-rotate(200deg)",
+        "photo": "Asset/HarshLamba.png",
+    },
+    {
+        "nameCostaa": "Divyam Chauhan",
+        "dept": "DEPARTMENT OF PAPER EVALUATIONS AND PRESENTATIONS",
+        "topRight": "hue-rotate(160deg)",
+        "gear": "hue-rotate(170deg)",
+        "photo": "Asset/Divyam.png",
+    }
+
+]
+
+function update() {
+    let d = data[currentSlide];
+    nameCostaa.textContent = d.nameCostaa;
+    dept.textContent = d.dept;
+    topRight.style.filter = d.topRight;
+    gear.style.filter = d.gear;
+    photo.src = d.photo;
 }
